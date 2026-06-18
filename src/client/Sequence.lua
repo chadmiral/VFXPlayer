e76ae4fd01e7649cae76184a930ce7c76ae1bb40
Chad Foxglove <=>
2026-06-18 12:15:46 -0700

    light support

--- src/client/Sequence.lua
+++ src/client/Sequence.lua
@@ -1,7 +1,8 @@
-Sequence = { name = "Sequence", _type = "Sequence", model = nil, startTime = -1, duration = -1, looping = false, particleDrivers = {} }
+Sequence = { name = "Sequence", _type = "Sequence", model = nil, startTime = -1, duration = -1, looping = false, particleDrivers = {}, lightDrivers = {} }
 
 local Utility = require(script.Parent:WaitForChild("Utility"))
 local ParticleDriver = require(script.Parent:WaitForChild("ParticleDriver"))
+local LightDriver = require(script.Parent:WaitForChild("LightDriver"))
 
 function Sequence:new(o)
     o = o or {}
@@ -38,6 +39,39 @@ local function initParticleEmitter(seq, e)
     return pd
 end
 
+local function initLight(seq, l)
+    local ld = LightDriver:new()
+    ld.light = l
+    ld.isSpotLight = l:IsA("SpotLight")
+
+    ld.duration = l:GetAttribute("Duration")
+    if ld.duration == nil then
+        ld.duration = seq.duration
+    end
+    ld.delay = l:GetAttribute("Delay")
+    if ld.delay == nil then
+        ld.delay = 0
+    end
+
+    ld.baseBrightness = l.Brightness
+    ld.brightnessScaleOverDuration = l:GetAttribute("BrightnessScaleOverDuration")
+
+    ld.baseRange = l.Range
+    ld.rangeScaleOverDuration = l:GetAttribute("RangeScaleOverDuration")
+
+    if ld.isSpotLight then
+        ld.baseAngle = l.Angle
+        ld.angleScaleOverDuration = l:GetAttribute("AngleScaleOverDuration")
+    end
+
+    ld.baseColor = l.Color
+    ld.tintOverDuration = l:GetAttribute("TintOverDuration")
+
+    ld:BeginCycle()
+
+    return ld
+end
+
 --reset all playing emitters to their starting states
 local function resetParticleDrivers(seq)
     for _,pd in seq.particleDrivers do
@@ -47,15 +81,32 @@ local function resetParticleDrivers(seq)
     end
 end
 
+--reset all playing lights to their starting states
+local function resetLightDrivers(seq)
+    for _,ld in seq.lightDrivers do
+        ld.light.Brightness = ld.baseBrightness
+        ld.light.Color = ld.baseColor
+        ld.light.Range = ld.baseRange
+        if ld.isSpotLight then
+            ld.light.Angle = ld.baseAngle
+        end
+    end
+end
+
 function Sequence:Init()
     resetParticleDrivers(self)
+    resetLightDrivers(self)
     self.particleDrivers = {}
+    self.lightDrivers = {}
 
     local descendants = self.model:GetDescendants()
     for _,d in descendants do
         if d:IsA("ParticleEmitter") then
             local pd = initParticleEmitter(self, d)
             table.insert(self.particleDrivers, pd)
+        elseif d:IsA("PointLight") or d:IsA("SpotLight") then
+            local ld = initLight(self, d)
+            table.insert(self.lightDrivers, ld)
         end
     end
 end
@@ -63,6 +114,10 @@ end
 function Sequence:Update(elapsedTime)
     for _,pd in self.particleDrivers do
         pd:Update(elapsedTime)
+    end
+
+    for _,ld in self.lightDrivers do
+        ld:Update(elapsedTime)
     end
 end
 
